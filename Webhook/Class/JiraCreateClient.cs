05a8025c5491c953b85a107fd7d297724f07c0c0
Paulo andrade <source.compu@gmail.com>
2018-12-20 06:31:48 -0600

    First commit

---
+++ Webhook/Class/JiraCreateClient.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Webhook.Class
+{
+    public class JiraCreateClient
+    {
+        /// <summary>
+        /// Creamos el cliente para Jira
+        /// </summary>
+        /// <param name="query">Url del API Rest</param>
+        /// <param name="argument"></param>
+        /// <param name="data"></param>
+        /// <param name="method">Metodo HTTP</param>
+        /// <returns></returns>
+        public static string RunQuery(string query, string data = null, string method = "GET")
+        {
+            try
+            {
+                string m_BaseUrl = query;
+                HttpWebRequest newRequest = WebRequest.Create(m_BaseUrl) as HttpWebRequest;
+                newRequest.ContentType = "application/json";
+                newRequest.Method = method;
+
+                // Verificamos si existen datos a enviar
+                if (data != null)
+                {
+                    using (StreamWriter writer = new StreamWriter(newRequest.GetRequestStream()))
+                    {
+                        writer.Write(data);
+                    }
+                }
+
+                // Codificamos en base 64 las credenciales de acceso
+                string base64Credentials = GetEncodedCredentials();
+                newRequest.Headers.Add("Authorization", "Basic " + base64Credentials);
+
+                // Enviamos la solicitud
+                HttpWebResponse response = newRequest.GetResponse() as HttpWebResponse;
+
+                string result = string.Empty;
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                }
+
+                newRequest = null;
+                response = null;
+
+                return result;
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show(@"There is a problem getting data from Jira :" + "\n\n" + query, "Jira Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Creamos y codificamos las credenciales de acceso
+        /// </summary>
+        /// <returns></returns>
+        private static string GetEncodedCredentials()
+        {
+            string m_Username = "paulo.gonzalez.sc@itszapopan.edu.mx";
+            string m_Password = "Paulo866";
+
+            string mergedCredentials = string.Format("{0}:{1}", m_Username, m_Password);
+            byte[] byteCredentials = UTF8Encoding.UTF8.GetBytes(mergedCredentials);
+            return Convert.ToBase64String(byteCredentials);
+        }
+    }
+}
---
+++ Webhook/Controllers/IssueController.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webhook.Class;
+
+namespace Webhook.Controllers
+{
+    public class IssueController : ApiController
+    {
+
+        // GET: api/Issue
+        public string Get()
+        {
+            // Obtenemos una busqueda generica
+            string result = JiraCreateClient.RunQuery("https://pandr01.atlassian.net/rest/api/latest/search");
+
+            dynamic json = JsonConvert.DeserializeObject(result);
+
+            return result;
+        }
+
+        // GET: api/Issue/5
+        public string Get(string key)
+        {
+            // Base uri
+            string uri = "https://pandr01.atlassian.net/rest/api/issue/";
+
+            // Obtenemos una issue predeterminada
+            string result = JiraCreateClient.RunQuery(uri+key);
+
+            dynamic json = JsonConvert.DeserializeObject(result);
+
+            return json.ToString();
+        }
+
+        // POST: api/Issue
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Issue/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Issue/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
